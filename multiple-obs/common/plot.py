fe45016b85600ab1e4ed2ab6c4b865e3ef8bcc46
Teodorus Nathaniel <nathankurnia26@gmail.com>
2022-04-24 00:37:59 +0700

    Add plot for last 100 episodes

--- multiple-obs/common/plot.py
+++ multiple-obs/common/plot.py
@@ -9,24 +9,30 @@ def plot_res(values, title=""):
     clear_output(wait=True)
 
     # Define the figure
-    f, ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
+    f, ax = plt.subplots(nrows=2, ncols=2, figsize=(12, 10))
     f.suptitle(title)
-    ax[0].plot(values, label="score per run")
-    ax[0].set_xlabel("Episodes")
-    ax[0].set_ylabel("Reward")
+    ax[0, 0].plot(values, label="score per run")
+    ax[0, 0].set_xlabel("Episodes")
+    ax[0, 0].set_ylabel("Reward")
     x = range(len(values))
-    ax[0].legend()
+    ax[0, 0].legend()
     # Calculate the trend
     try:
         z = np.polyfit(x, values, 1)
         p = np.poly1d(z)
-        ax[0].plot(x, p(x), "--", label="trend")
+        ax[0, 0].plot(x, p(x), "--", label="trend")
     except:
         print("")
 
     # Plot the histogram of results
-    ax[1].hist(values[-50:])
-    ax[1].set_xlabel("Scores per Last 50 Episodes")
-    ax[1].set_ylabel("Frequency")
-    ax[1].legend()
+    ax[0, 1].hist(values[-50:])
+    ax[0, 1].set_xlabel("Scores per Last 50 Episodes")
+    ax[0, 1].set_ylabel("Frequency")
+    ax[0, 1].legend()
+
+    # Plot last 100
+    ax[1, 0].plot(values[-100:])
+    ax[1, 0].set_xlabel("Episodes")
+    ax[1, 0].set_ylabel("Reward")
+    ax[1, 0].legend()
     plt.show()
